a3bbc72e2186f8b3c07fe7e2bab684fba1117977
finnporter <iamfinnporter@gmail.com>
2017-07-11 15:39:01 +0100

    adds visitor class

---
+++ src/zoo_mgmt/Visitor.java
@@ -0,0 +1,29 @@
+package zoo_mgmt;
+
+public class Visitor {
+	
+	private String name;
+	private double cash;
+	private int age;
+	
+	public Visitor(String name, double cash, int age) {
+		this.name = name;
+		this.cash = cash;
+		this.age = age;
+	}
+	
+//GETTERS
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getCash() {
+		return this.cash;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+
+}
---
+++ src/zoo_mgmt_spec/VisitorTest.java
@@ -0,0 +1,31 @@
+package zoo_mgmt_spec;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import zoo_mgmt.Visitor;
+
+public class VisitorTest {
+	Visitor visitor;
+	
+	@Before
+	public void before() {
+		visitor = new Visitor("Hawkeye Pierce", 200, 36);
+	}
+	
+	@Test
+	public void hasName() {
+		assertEquals("Hawkeye Pierce", visitor.getName());
+	}
+	
+	@Test
+	public void hasCash() {
+		assertEquals(200, visitor.getCash(), 0.01);
+	}
+	
+	@Test
+	public void hasAge() {
+		assertEquals(36, visitor.getAge());
+	}
+}
