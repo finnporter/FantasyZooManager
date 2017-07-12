73f11aee8a92f5c6b57f0a8f9a42d77b2c7ede83
finnporter <iamfinnporter@gmail.com>
2017-07-12 14:36:56 +0100

    fixes imports on hydra.java

--- src/zoo_mgmt/animals/Hydra.java
+++ src/zoo_mgmt/animals/Hydra.java
@@ -1,10 +1,7 @@
 package zoo_mgmt.animals;
 
-import org.junit.Test;
-
 import zoo_mgmt.Animal;
 import zoo_mgmt.AnimalType;
-import zoo_mgmt.EncType;
 import zoo_mgmt.TemporalSpecialisation;
 
 public class Hydra extends Animal{
